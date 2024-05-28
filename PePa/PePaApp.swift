c947e7c3d5142969ffd71d0ef1add2d44eb2d325
Akemi Sai <126123381+achemai@users.noreply.github.com>
2024-05-28 16:19:26 -0400

    Initial Commit

---
+++ PePa/PePaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PePaApp.swift
+//  PePa
+//
+//  Created by Akemi Sai on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PePaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
